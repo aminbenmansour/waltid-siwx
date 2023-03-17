5c3bd30e22b17dfa8ac599a0cbff31d5d4d9d541
aminbenmansour <amin.benmansour.10@gmail.com>
2023-03-17 16:12:59 +0100

    creating authentication context

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,180 @@
+import {
+  createContext,
+  ReactNode,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+
+import AuthClient from "@walletconnect/auth-client";
+import { SessionTypes } from "@walletconnect/types";
+import {
+  Cacao,
+  getAppMetadata,
+  formatMessage,
+  getNamespacedDidChainId,
+  getDidAddress,
+} from "@walletconnect/utils";
+
+import { DEFAULT_APP_METADATA } from "../constants";
+import { createRequestParams } from "../helpers/caip122";
+import { useWalletConnectClient } from "./ClientContext";
+
+/**
+ * Types
+ */
+
+interface IContext {
+  authClient: AuthClient | undefined;
+  authHasInitialized: boolean;
+  authUri: string;
+  signIn: (_session: SessionTypes.Struct) => void;
+}
+
+// request: (
+//   params: RequestParams,
+//   topic: string
+// ) => Promise<{ uri: any; id: any }>;
+// respond: (params: RespondParams, iss: string) => Promise<boolean>;
+// getPendingRequests: () => Promise<Record<number, PendingRequest>>;
+// formatMessage: (payload: PayloadParams, iss: string) => Promise<string>;
+
+/**
+ * Context
+ */
+export const AuthContext = createContext<IContext>({} as IContext);
+
+/**
+ * Provider
+ */
+export function AuthContextProvider({
+  children,
+}: {
+  children: ReactNode | ReactNode[];
+}) {
+  const [authClient, setClient] = useState<AuthClient>();
+  const [authHasInitialized, setHasInitialized] = useState(false);
+  const [authUri, setUri] = useState<string>("");
+  const [addresses, setAddresses] = useState<string[]>([]);
+
+  const { session } = useWalletConnectClient();
+
+  const _subscribeToEvents = useCallback(async (_client: AuthClient) => {
+    if (typeof _client === "undefined") {
+      throw new Error("Authentication Client is not initialized");
+    }
+    _client.on("auth_request", ({ params }) => {
+      console.log("Authentication Request", params);
+    });
+
+    _client.on("auth_response", ({ params }) => {
+      console.log("Authentication Response: ", params);
+    });
+  }, []);
+
+  const createClient = useCallback(() => {
+    AuthClient.init({
+      relayUrl:
+        process.env.NEXT_PUBLIC_RELAY_URL || "wss://relay.walletconnect.com",
+      projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
+      metadata: getAppMetadata() || DEFAULT_APP_METADATA,
+    })
+      .then(async (authClient) => {
+        setClient(authClient);
+        setHasInitialized(true);
+        console.log("CREATED AUTH CLIENT: ", authClient);
+        await _subscribeToEvents(authClient);
+      })
+      .catch(() => {
+        setHasInitialized(false);
+        console.error;
+      });
+  }, [_subscribeToEvents]);
+
+  const signIn = useCallback(
+    (_session: SessionTypes.Struct) => {
+      if (!authClient) return;
+
+      const topic: string = _session.topic;
+
+      Object.keys(_session.namespaces).map((namespace) => {
+        console.log(`Sign In to Namespace: ${namespace}`);
+        _session.namespaces[namespace].accounts.map(async (account) => {
+          const [chainId, address] = [
+            account.split(":")[1],
+            account.split(":")[2],
+          ];
+          const request = createRequestParams(chainId);
+          console.log("")
+          const response: Cacao | void = await authClient
+            .request(request, {
+              topic,
+            })
+            .then(async ({ uri }) => {
+              if (uri) {
+                setUri(uri);
+                setAddresses((addresses) => {
+                  console.log("This Address is AUTHENTICATED WIWOUU: ", address)
+                  addresses.push(address);
+                  return addresses;
+                });
+              }
+            });
+
+          if (response instanceof Object) {
+            const iss = response.p.iss;
+            console.log(
+              "Authentication successful for :",
+              getNamespacedDidChainId(iss),
+              "for address: ",
+              getDidAddress(iss)
+            );
+            console.log(
+              "signed the following message: ",
+              formatMessage(response.p, iss)
+            );
+          }
+        });
+      });
+    },
+    [authClient, setUri, setAddresses]
+  );
+
+  useEffect(() => {
+    if (!authClient) {
+      createClient();
+    }
+  }, [authClient, createClient]);
+
+  const value = useMemo(
+    () => ({
+      authClient,
+      authHasInitialized,
+      authUri,
+      signIn,
+    }),
+    [authClient, authHasInitialized, authUri, signIn]
+  );
+
+  return (
+    <AuthContext.Provider
+      value={{
+        ...value,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useWCAuthClient = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error(
+      "useWCAuthClient must be used within a AuthContextProvider"
+    );
+  }
+  return context;
+};
